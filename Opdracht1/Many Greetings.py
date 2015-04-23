c80ced09cc56df03066140c6bce6620f2e3ff1ad
wesselvaneeghen <wessel_van_eeghen@hotmail.com>
2015-04-23 13:34:16 +0000

    Many Greetings.py

--- Opdracht1/Many Greetings.py
+++ Opdracht1/Many Greetings.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 x=input("What is your name? ")
 y=input("How many greetings? ")
 print(y+" x "+"Hello, "+x+"!")
-=======
-x=input("Name? ")
-y=input("How many greetings? ")
-print(y+" x "+"Hello, "+x+"!")
->>>>>>> extension
