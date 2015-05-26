dc7980aca5d3bd017131b6d9db10684f44a41466
wesselvaneeghen <wessel_van_eeghen@hotmail.com>
2015-05-26 09:38:14 +0000

    Vector.py

--- Opdracht4/bisection.py
+++ Opdracht4/bisection.py
@@ -14,3 +14,25 @@ def findRoot(f,a,b,epsilon):
             return findRoot(f,a,m,epsilon)
         elif float(f(b))>0:
             return findRoot(f,m,b,epsilon)
+
+list_root = []
+
+def findAllRoots(f,a,b,epsilon):
+    a, b = [min(a,b), max(a,b)]
+    m = float(0.5*(a+b))
+    print(m)
+    input()
+    if abs(float(f(m))) <= 10**(-6):
+        list_root.append(m)
+        return list_root
+    if ((abs(b-a)<epsilon) and (a<0) and (b>0)):
+        list_root.append(m)
+        return list_root
+    elif abs(b-a)<epsilon and b<0:  #geen oplossing
+        return
+    elif abs(b-a)<epsilon and a>0:  #geen oplossing
+        return
+    else:
+        findAllRoots(f,a,m,epsilon)
+        findAllRoots(f,m,b,epsilon)
+    return list_root
